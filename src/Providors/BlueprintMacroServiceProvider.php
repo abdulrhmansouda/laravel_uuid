d4cf2399d97dcd625591e77555de35b5d2f6d14d
Abdul Rahman Souda <eng.abdul.rahman19992@gmail.com>
2023-08-15 10:19:47 +0300

    add muid for blueprint using macro providor

---
+++ src/Providors/BlueprintMacroServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MUID\Providers;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
+
+class BlueprintMacroServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blueprint::macro('muid', function ($name = 'muid') {
+            $this->string($name)
+            ->unique();
+        });
+    }
+}
